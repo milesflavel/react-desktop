f782812751b6170a050d4306e844305df6151d13
Miles Flavel <milesflavel@gmail.com>
2026-06-06 15:41:03 +0800

    Add click and drag hook for window dragging

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useClickAndDrag } from "../hooks/useClickAndDrag";
 
 interface WindowProps {
   title?: string;
@@ -6,17 +7,25 @@ interface WindowProps {
 }
 
 export const Window = ({ title, children }: WindowProps) => {
-  const [width, setWidth] = useState(400);
-  const [height, setHeight] = useState(300);
+  const [width, _setWidth] = useState(400);
+  const [height, _setHeight] = useState(300);
   const [x, setX] = useState(200);
   const [y, setY] = useState(300);
+
+  const { ref: headerRef } = useClickAndDrag((posX, posY) => {
+    setX(posX);
+    setY(posY);
+  });
 
   return (
     <section
       className="border-ridge bg-neutral-350 absolute flex flex-col border-4 border-t-neutral-200 border-r-neutral-400 border-b-neutral-400 border-l-neutral-200"
       style={{ width: width, height: height, left: x, top: y }}
     >
-      <header className="flex h-6 w-full items-center bg-linear-to-r from-blue-900 to-blue-700 p-1">
+      <header
+        ref={headerRef}
+        className="flex h-6 w-full cursor-default items-center bg-linear-to-r from-blue-900 to-blue-700 p-1"
+      >
         <h1 className="text-lg/none font-medium text-white">{title}</h1>
       </header>
       <section className="grow">{children}</section>
---
+++ src/hooks/useClickAndDrag.ts
@@ -0,0 +1,88 @@
+import { useEffect, useRef } from "react";
+
+export const useClickAndDrag = (
+  onDrag: (posX: number, posY: number) => void,
+  dragThreshold: number = 5,
+) => {
+  const elementRef = useRef<HTMLElement>(null);
+  const isDragging = useRef(false);
+  const dragStartX = useRef(0);
+  const dragStartY = useRef(0);
+  const elementStartX = useRef(0);
+  const elementStartY = useRef(0);
+
+  useEffect(() => {
+    const element = elementRef.current;
+
+    const mousedownHandler = (e: MouseEvent) => {
+      e.preventDefault();
+
+      isDragging.current = false;
+      dragStartX.current = e.pageX;
+      dragStartY.current = e.pageY;
+
+      if (element) {
+        const elementRect = element.getBoundingClientRect();
+        elementStartX.current = elementRect.left + window.scrollX;
+        elementStartY.current = elementRect.top + window.scrollY;
+
+        document.addEventListener("mousemove", mousemoveHandler);
+        document.addEventListener("mouseup", mouseupHandler);
+      }
+    };
+
+    const mousemoveHandler = (e: MouseEvent) => {
+      const dx = e.pageX - dragStartX.current;
+      const dy = e.pageY - dragStartY.current;
+
+      if (!isDragging.current) {
+        const distance = Math.hypot(dx, dy);
+
+        if (distance > dragThreshold) {
+          isDragging.current = true;
+        }
+      } else {
+        onDrag(elementStartX.current + dx, elementStartY.current + dy);
+      }
+    };
+
+    const mouseupHandler = (e: MouseEvent) => {
+      if (isDragging.current) {
+        e.preventDefault();
+      }
+
+      if (element) {
+        document.removeEventListener("mousemove", mousemoveHandler);
+        document.removeEventListener("mouseup", mouseupHandler);
+      }
+
+      isDragging.current = false;
+      dragStartX.current = 0;
+      dragStartY.current = 0;
+      elementStartX.current = 0;
+      elementStartY.current = 0;
+    };
+
+    if (element) {
+      element.addEventListener("mousedown", mousedownHandler);
+    }
+
+    return () => {
+      if (element) {
+        element.removeEventListener("mousedown", mousedownHandler);
+        document.removeEventListener("mousemove", mousemoveHandler);
+        document.removeEventListener("mouseup", mouseupHandler);
+      }
+
+      isDragging.current = false;
+      dragStartX.current = 0;
+      dragStartY.current = 0;
+      elementStartX.current = 0;
+      elementStartY.current = 0;
+    };
+  }, [elementRef.current, dragThreshold]);
+
+  return {
+    ref: elementRef,
+  };
+};
